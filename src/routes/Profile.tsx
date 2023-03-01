88f8a0cb10fbb752bc16d9c6054cfa05ddada045
IvaTsu <ivan.tsud@1337.tech>
2023-03-01 10:55:16 +0100

    feat: create Profile route

---
+++ src/routes/Profile.tsx
@@ -0,0 +1,35 @@
+import "../App.css";
+
+import NavigationBar from "../components/NavigationBar";
+import ProtectedRoute from "../routes/ProtectedRoute";
+import { useSessionStore, useUserStore } from "../store";
+
+function Profile(): JSX.Element {
+  const { clear: clearSession } = useSessionStore();
+  const { clear: clearUser, getUser } = useUserStore();
+
+  const user = getUser();
+
+  const _onSignOutClick = (): void => {
+    clearSession();
+    clearUser();
+  };
+
+  return (
+    <ProtectedRoute>
+      <>
+        <NavigationBar />
+        <div className="App">
+          <div>
+            <h1>Hello, {user?.name}</h1>
+            <img src={user?.picture} referrerPolicy="no-referrer"></img>
+            <p>{user?.email}</p>
+            <button onClick={_onSignOutClick}>Sign Out</button>
+          </div>
+        </div>
+      </>
+    </ProtectedRoute>
+  );
+}
+
+export default Profile;
