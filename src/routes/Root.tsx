e7e84ae44de2c408c0e633892cc88914b8a1829a
IvaTsu <ivan.tsud@1337.tech>
2023-03-02 11:57:52 +0100

    feat: fetch players list

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,7 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { fetchPlayersList } from "../api/queries";
 import NavigationBar from "../components/NavigationBar";
 import ProtectedRoute from "../routes/ProtectedRoute";
 
 function Root(): JSX.Element {
+  const { data: playersList } = useQuery(["playersList"], fetchPlayersList);
+
   return (
     <ProtectedRoute>
       <>
@@ -15,30 +20,21 @@ function Root(): JSX.Element {
                 <th>Name</th>
                 <th>Rating</th>
                 <th>Office</th>
+                <th>Games played</th>
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
-              <tr className="hover">
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>1400</td>
-                <td>Stockholm</td>
-              </tr>
-              {/* row 2 */}
-              <tr className="hover">
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>1820</td>
-                <td>Stockholm</td>
-              </tr>
-              {/* row 3 */}
-              <tr className="hover">
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>1300</td>
-                <td>Lund</td>
-              </tr>
+              {playersList?.content.map((player, index) => {
+                return (
+                  <tr key={player.id} className="hover">
+                    <th>{index + 1}</th>
+                    <td>{player.name}</td>
+                    <td>{player.rating}</td>
+                    <td>{player.tournamentRef.name}</td>
+                    <td>{player.gamesPlayed}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
