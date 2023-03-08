2377d9861659749beeb12dd23d753d893414d022
IvaTsu <ivan.tsud@1337.tech>
2023-03-08 11:49:39 +0100

    feat: add loading indication for ProtectedRoute

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,8 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router-dom";
 
+import { LoadingSpinner } from "../components/LoadingSpinner";
+
 function ProtectedRoute({ children }: { children: JSX.Element }): JSX.Element {
   const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
 
   if (!isLoading && !isAuthenticated) {
     return <Navigate to="/login" replace />;
