acbdb266b537a710f7babac43eda2fbc968ee783
IvaTsu <ivan.tsud@1337.tech>
2023-03-09 13:26:58 +0100

    feat: add GET & API handlers, GET & POST player, GET tournament

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -1,10 +1,48 @@
-import { type IPlayerListParams, playersList } from "./urls";
+import {
+  type IPlayerFindParams,
+  type IPlayerListParams,
+  playerCreate,
+  playerFind,
+  playersList,
+  tournamentList,
+} from "./urls";
 
-async function request<T>(url: string): Promise<T> {
+interface IGetRequestParams {
+  accessToken: string;
+  url: string;
+}
+
+async function getRequest<T>({
+  accessToken,
+  url,
+}: IGetRequestParams): Promise<T> {
   const response = await fetch(url, {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
     },
+  });
+
+  return await response.json();
+}
+
+interface IPostRequestParams extends IGetRequestParams {
+  body: object;
+}
+
+async function postRequest<T>({
+  accessToken,
+  body,
+  url,
+}: IPostRequestParams): Promise<T> {
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(body),
   });
   return await response.json();
 }
@@ -34,8 +72,57 @@ interface IPlayersList {
 }
 
 export async function fetchPlayersList({
+  accessToken,
   page,
   size,
-}: IPlayerListParams): Promise<IPlayersList> {
-  return await request(playersList({ page, size }));
+}: IPlayerListParams & { accessToken: string }): Promise<IPlayersList> {
+  return await getRequest({ accessToken, url: playersList({ page, size }) });
+}
+
+export interface IPlayer {
+  id: string;
+  name: string;
+  email: string;
+  profileImage: string;
+  tournamentRef: {
+    id: string;
+    name: string;
+  };
+  rating: number;
+  registeredWhen: string;
+  gamesPlayed: number;
+}
+
+export async function fetchPlayer({
+  accessToken,
+  name,
+  tournamentId,
+}: IPlayerFindParams & { accessToken: string }): Promise<IPlayer[]> {
+  return await getRequest({
+    accessToken,
+    url: playerFind({ name, tournamentId }),
+  });
+}
+
+export interface ITournamentList {
+  id: string;
+  name: string;
+}
+
+export async function fetchTournamentList(
+  accessToken: string
+): Promise<ITournamentList[]> {
+  return await getRequest({ accessToken, url: tournamentList() });
+}
+
+interface ICreatePlayerParams {
+  accessToken: string;
+  body: object;
+}
+
+export async function createPlayer({
+  accessToken,
+  body,
+}: ICreatePlayerParams): Promise<IPlayer> {
+  return await postRequest({ accessToken, body, url: playerCreate() });
 }
--- src/api/urls.ts
+++ src/api/urls.ts
@@ -7,3 +7,21 @@ export interface IPlayerListParams {
 
 export const playersList = ({ page, size }: IPlayerListParams): string =>
   `${baseURL}/player/list?page=${page}&size=${size}`;
+
+export interface IPlayerFindParams {
+  name: string;
+  tournamentId?: string;
+}
+
+export const playerFind = ({
+  name,
+  tournamentId,
+}: IPlayerFindParams): string => {
+  return tournamentId != null
+    ? `${baseURL}/player/find?name=${name}&tournamentId=${tournamentId}`
+    : `${baseURL}/player/find?name=${name}`;
+};
+
+export const tournamentList = (): string => `${baseURL}/tournament/list`;
+
+export const playerCreate = (): string => `${baseURL}/player/create`;
