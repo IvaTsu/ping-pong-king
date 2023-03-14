ec7ddb2d9c9e29b20fb4d4b5b4d7086163b2ab1a
Ivan Tsud <ivan.tsud@1337.tech>
2023-03-14 16:37:29 +0200

    refactor: generic body type (#21)
    * refactor: use generic body typing for the POST requests
    
    * refactor(api/game): use generic type for POST /game body
    
    * refactor(api/player): use generic type for POST /player body

--- src/api/game/post/types.ts
+++ src/api/game/post/types.ts
@@ -22,7 +22,20 @@ export interface IGame {
   playedWhen: string;
 }
 
-export interface ICreateGameParams {
-  accessToken: string;
-  body: object;
+export interface IPostGameBody {
+  playerRefA: {
+    id: string;
+  };
+  playerRefB: {
+    id: string;
+  };
+  // TODO: this should be a separate selector
+  tournamentRef: {
+    id: string;
+  };
+  gameResult: {
+    playerAScore: number;
+    playerBScore: number;
+    winnerId: string;
+  };
 }
