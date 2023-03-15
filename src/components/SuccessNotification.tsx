c6e0ec1c5a0ed776a7c09547fbeb090dc4961a09
vitdod <vitdod31@gmail.com>
2023-03-15 10:26:27 +0100

    fixed successNotification first render issue

--- src/components/SuccessNotification.tsx
+++ src/components/SuccessNotification.tsx
@@ -5,19 +5,20 @@ interface SuccessNotificationProps {
 export default function SuccessNotification({
   isOpen,
 }: SuccessNotificationProps): JSX.Element {
-  const [opacity, setOpacity] = useState<number>(0);
+  console.log(isOpen);
+  const [opacity, setOpacity] = useState<string>("opacity-0");
   useEffect(() => {
-    isOpen ? setOpacity(100) : setOpacity(0);
+    isOpen ? setOpacity("opacity-100") : setOpacity("opacity-0");
     return () => {
       setTimeout(() => {
-        setOpacity(0);
+        setOpacity("opacity-0");
       }, 3000);
     };
   }, [isOpen]);
 
   return (
     <div
-      className={`absolute top-0 right-0 mt-3 p-2 shadow bg-base-100 rounded-box w-52 transition  transform opacity-${opacity}`}
+      className={`absolute top-0 right-0 mt-3 p-2 shadow bg-base-100 rounded-box w-52 transition transform ${opacity}`}
     >
       The game was added succesfully!
     </div>
