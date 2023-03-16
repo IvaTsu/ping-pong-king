49d3cc4fa73f76772733c60f6059dac384cd59b2
vitdod <vitdod31@gmail.com>
2023-03-16 10:52:04 +0100

    deleted log

--- src/components/SuccessNotification.tsx
+++ src/components/SuccessNotification.tsx
@@ -9,7 +9,6 @@ export default function SuccessNotification({
   useEffect(() => {
     isOpen ? setOpacity("opacity-100") : setOpacity("opacity-0");
   }, [isOpen]);
-  console.log(isOpen);
   return (
     <div
       className={`absolute top-0 right-0 mt-3 p-2 shadow bg-base-100 rounded-box w-52 transition transform ${opacity}`}
