a5719c9b029bf9997b38fec1c6d9c886e8d34831
IvaTsu <ivan.tsud@1337.tech>
2023-04-03 16:39:40 +0200

    feat(PlayerHistory): fetch all the games by the user id

--- src/routes/PlayerHistory.tsx
+++ src/routes/PlayerHistory.tsx
@@ -1,16 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
 import { useLocation, useParams } from "react-router-dom";
+
+import { fetchGamesByUserId } from "../api/game/get/queries";
+import { fiveMinutes } from "../constants/time";
+import { useAuthStore } from "../store";
 
 const PlayerHistory = (): JSX.Element => {
   const location = useLocation();
-  const params = useParams();
-
   const { playerId } = location.state;
+
+  const params = useParams();
   const { playerName } = params;
+
+  const { getAuth } = useAuthStore();
+  const auth = getAuth();
+
+  const { data: games } = useQuery(
+    ["playerHistory", playerId],
+    async () => await fetchGamesByUserId(auth?.accessToken as string, playerId),
+    {
+      enabled: auth?.accessToken != null,
+      staleTime: fiveMinutes,
+    }
+  );
 
   return (
     <>
       <p>{playerId}</p>
       <p>{playerName}</p>
+      <pre>{JSON.stringify(games)}</pre>
     </>
   );
 };
