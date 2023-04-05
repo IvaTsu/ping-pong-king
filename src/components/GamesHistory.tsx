dc1f5e01be7e0e09e9364f9db6b53f063fd7365d
IvaTsu <ivan.tsud@1337.tech>
2023-04-05 12:00:12 +0200

    refactor(GamesHistory): use paginated table for games history display

--- src/components/GamesHistory.tsx
+++ src/components/GamesHistory.tsx
@@ -1,124 +1,284 @@
 import { useQuery } from "@tanstack/react-query";
-import { useMemo } from "react";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  type PaginationState,
+  type SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useMemo, useState } from "react";
 
 import { fetchGamesByUserId } from "../api/game/get/queries";
+import { type IGame } from "../api/game/types";
 import { fiveMinutes } from "../constants/time";
 import { useAuthStore, useUserStore } from "../store";
-import getRandomEmoji from "../utils/getRandomEmoji";
 import { LoadingSpinner } from "./LoadingSpinner";
 
+const columnHelper = createColumnHelper<IGame>();
+
 export const GamesHistory = (): JSX.Element => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
   const { getAuth } = useAuthStore();
   const auth = getAuth();
 
   const { getUser } = useUserStore();
   const currentUser = getUser();
 
+  const pagination = useMemo(
+    () => ({
+      pageIndex,
+      pageSize,
+    }),
+    [pageIndex, pageSize]
+  );
+
   const { data: gameList, isLoading: isGameListLoading } = useQuery(
-    ["gamesByUserId", currentUser?.id, fetchGamesByUserId, auth?.accessToken],
+    [
+      "gamesByUserId",
+      currentUser?.id,
+      fetchGamesByUserId,
+      auth?.accessToken,
+      pagination,
+    ],
     async () =>
       await fetchGamesByUserId({
         accessToken: auth?.accessToken as string,
         id: currentUser?.id as string,
-        page: 0,
-        size: 40,
+        page: pageIndex,
+        size: pageSize,
       }),
     {
       enabled: auth?.accessToken != null && currentUser?.id != null,
       staleTime: fiveMinutes,
+      keepPreviousData: true,
+      retry: false,
     }
   );
 
-  const gameHistory = useMemo(() => {
-    return gameList?.content.map((game) => {
-      const date = new Date(game.playedWhen).toISOString().split("T")[0];
-      const { gameResult, playerRefB, playerRefA } = game;
-      const { winnerId } = gameResult;
-      const opponentName =
-        playerRefB.name === currentUser?.name
-          ? playerRefA.name
-          : playerRefB.name;
-      const id = game.id;
-      const isWinner = winnerId === currentUser?.id;
-      return {
-        opponentName,
-        won: isWinner,
-        score: `${gameResult.playerAScore} : ${gameResult.playerBScore}`,
-        gainOrLoss: isWinner
+  const userColumnDefs = [
+    columnHelper.accessor(
+      (row) =>
+        row.playerRefB.id === currentUser?.id
+          ? row.playerRefA.name
+          : row.playerRefB.name,
+      {
+        id: "Opponent",
+        cell: (info) => <span>{info.getValue()}</span>,
+        header: () => <span>Opponent</span>,
+      }
+    ),
+    columnHelper.accessor(
+      (row) => row.gameResult.winnerId === currentUser?.id,
+      {
+        id: "result",
+        cell: (info) => <span>{JSON.stringify(info.getValue())}</span>,
+        header: () => <span>Result</span>,
+      }
+    ),
+    columnHelper.accessor(
+      (row) =>
+        `${row.gameResult.playerAScore} : ${row.gameResult.playerBScore}`,
+      {
+        id: "score",
+        cell: (info) => <span>{info.getValue()}</span>,
+        header: () => <span>Score</span>,
+      }
+    ),
+    columnHelper.accessor(
+      (row) =>
+        row.gameResult.winnerId === currentUser?.id
           ? Math.max(
-              gameResult.playerARatingAlteration,
-              gameResult.playerBRatingAlteration
+              row.gameResult.playerARatingAlteration,
+              row.gameResult.playerBRatingAlteration
             )
           : Math.min(
-              gameResult.playerARatingAlteration,
-              gameResult.playerBRatingAlteration
+              row.gameResult.playerARatingAlteration,
+              row.gameResult.playerBRatingAlteration
             ),
-        date,
-        id,
-      };
-    });
-  }, [gameList?.content]);
+      {
+        id: "gainOrLoss",
+        cell: (info) => <span>{info.getValue()}</span>,
+        header: () => <span>Gain / Loss</span>,
+      }
+    ),
+    columnHelper.accessor(
+      (row) => new Date(row.playedWhen).toISOString().split("T")[0],
+      {
+        id: "date",
+        cell: (info) => <span>{info.getValue()}</span>,
+        header: () => <span>Date</span>,
+      }
+    ),
+  ];
+
+  const table = useReactTable({
+    columns: userColumnDefs,
+    data: gameList?.content ?? [],
+    pageCount: gameList?.pageable.totalPages,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+      pagination,
+    },
+    onSortingChange: setSorting,
+    onPaginationChange: setPagination,
+    manualPagination: true,
+  });
+
+  const headers = table.getFlatHeaders();
+  const rows = table.getRowModel().rows;
+  const currentPage = table.getState().pagination.pageIndex;
 
   return (
     <>
-      {gameHistory != null && gameHistory.length > 0 ? (
-        <div className="mt-10 w-full flex items-start border-lightGrey flex-col sm:w-3/4">
+      {gameList != null && gameList.content.length > 0 ? (
+        <>
           <p className="text-2xl font-ubuntuBold  text-navy dark:text-aqua mb-3 ml-10">
             Game history
           </p>
-          <div className="card card-side bg-base-100 shadow-xl w-full flex flex-col p-2 sm:p-2 max-h-96 overflow-auto text-xs sm:text-base border border-lightGrey rounded dark:border-0">
-            <div className="flex w-full items-start justify-start text-center border-b-2 border-cloud dark:border-lightGrey">
-              {["Opponent", "Result", "Score", "Gain/Loss", "Date"].map(
-                (header) => {
-                  return (
-                    <p
-                      className="w-1/4 font-ubuntuBold mb-2 text-center"
-                      key={header}
+          <div className="overflow-x-auto py-10">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  {headers.map((header) => {
+                    const direction = header.column.getIsSorted();
+                    const arrow = {
+                      asc: "🔼",
+                      desc: "🔽",
+                    };
+                    const sortIndicator =
+                      (direction === "asc" || direction === "desc") &&
+                      arrow[direction];
+                    return (
+                      <th key={header.id}>
+                        {header.isPlaceholder ? null : (
+                          <div
+                            onClick={header.column.getToggleSortingHandler()}
+                            className="cursor-pointer flex gap-4 font-ubuntuBold"
+                          >
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                            {direction !== false && (
+                              <span>{sortIndicator}</span>
+                            )}
+                          </div>
+                        )}
+                      </th>
+                    );
+                  })}
+                </tr>
+              </thead>
+              <tbody>
+                {rows.map((row) => (
+                  <tr key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className="my-2">
+              <div className="flex items-start sm:items-center flex-col sm:flex-row  gap-2">
+                <div className="btn-group flex justify-between sm:justify-start w-full sm:w-fit">
+                  <div>
+                    <button
+                      className="btn btn-sm"
+                      onClick={() => {
+                        table.setPageIndex(0);
+                      }}
+                      disabled={!table.getCanPreviousPage()}
                     >
-                      {header}
-                    </p>
-                  );
-                }
-              )}
-            </div>
-            <div className="p-2 flex flex-col">
-              {gameHistory.map((game) => {
-                const { opponentName, won, date, score, gainOrLoss } = game;
-                return (
-                  <div
-                    className="flex w-full items-start justify-start text-center py-2 border-b border-darkGrey  dark:border-lightGrey"
-                    key={game.id}
-                  >
-                    <p className="w-1/5">{opponentName}</p>
-                    <div className="w-1/5 flex justify-center">
-                      {won ? (
-                        <img src="check.svg" className="w-6" />
-                      ) : (
-                        <img src="denied.svg" className="w-6 color-rose" />
-                      )}
-                    </div>
-                    <p className="w-1/5">{score}</p>
-                    <p className="w-1/5">
-                      {isNaN(gainOrLoss) ? (
-                        <span aria-hidden="true" role="img">
-                          {getRandomEmoji()}
-                        </span>
-                      ) : gainOrLoss > 0 ? (
-                        <span className="text-middleGreen">{gainOrLoss}</span>
-                      ) : (
-                        <span className="text-rose">{gainOrLoss}</span>
-                      )}
-                    </p>
-                    <p className="w-1/5">{date}</p>
+                      {"<<"}
+                    </button>
+                    <button
+                      className="btn btn-sm"
+                      onClick={() => {
+                        table.previousPage();
+                      }}
+                      disabled={!table.getCanPreviousPage()}
+                    >
+                      {"<"}
+                    </button>
+                    <button
+                      className="btn btn-sm"
+                      onClick={() => {
+                        table.nextPage();
+                      }}
+                      disabled={!table.getCanNextPage()}
+                    >
+                      {">"}
+                    </button>
+                    <button
+                      className="btn btn-sm"
+                      onClick={() => {
+                        table.setPageIndex(table.getPageCount() - 1);
+                      }}
+                      disabled={!table.getCanNextPage()}
+                    >
+                      {">>"}
+                    </button>
                   </div>
-                );
-              })}
+                  <span className="flex items-center gap-1 ml-1">
+                    <div>Page</div>
+                    <strong>
+                      {table.getState().pagination.pageIndex + 1} of{" "}
+                      {table.getPageCount()}
+                    </strong>
+                  </span>
+                </div>
+                <div className="flex">
+                  <span className="flex items-center gap-1 before:content-[''] sm:before:content-['|_'] ">
+                    Go to page:
+                    <input
+                      defaultValue={currentPage + 1}
+                      type="number"
+                      onChange={(e) => {
+                        const page =
+                          e.target.value != null
+                            ? Number(e.target.value) - 1
+                            : 0;
+                        table.setPageIndex(page);
+                      }}
+                      className="input input-bordered w-20 input-sm mx-2 focus:outline-none dark:focus:border-aqua focus:border-navy"
+                    />
+                  </span>
+                  <select
+                    value={table.getState().pagination.pageSize}
+                    onChange={(e) => {
+                      table.setPageSize(Number(e.target.value));
+                    }}
+                    className="select select-sm select-bordered focus:outline-none dark:focus:border-aqua focus:border-navy"
+                  >
+                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                      <option key={pageSize} value={pageSize}>
+                        Show {pageSize}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       ) : !isGameListLoading &&
-        gameHistory != null &&
-        gameHistory.length === 0 ? (
+        gameList != null &&
+        gameList.content.length === 0 ? (
         <div className="card flex justify-center bg-aqua dark:bg-cloudBirst w-full sm:w-96 h-16 mt-10">
           You haven&apos;t played any games yet!
         </div>
--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -52,77 +52,75 @@ function Profile(): JSX.Element {
     <ProtectedRoute>
       <>
         <NavigationBar />
-        <div className="flex items-center flex-col">
-          {currentUser == null ? (
-            <div>
-              <h1>Hello, {userFromIdToken?.name}</h1>
-              <p className="my-5">Assign yourself to the Office</p>
+        {currentUser == null ? (
+          <div>
+            <h1>Hello, {userFromIdToken?.name}</h1>
+            <p className="my-5">Assign yourself to the Office</p>
 
-              <select
-                className="mb-5 mr-5 select select-info w-full max-w-xs"
-                defaultValue={"DEFAULT"}
-                onChange={_onTournamentChange}
-              >
-                <option value={"DEFAULT"} disabled>
-                  Select office
-                </option>
-                {tournamentList?.map((tournament) => {
-                  return (
-                    <option key={tournament.id} value={tournament.id}>
-                      {tournament.name}
-                    </option>
-                  );
-                })}
-              </select>
-              <button
-                className={`btn btn-ghost ${
-                  tournamentId != null
-                    ? "btn-outline btn-success"
-                    : "btn-disabled"
-                }  ${isTournamentListLoading ? "loading" : ""}`}
-                onClick={() => {
-                  if (
-                    auth?.accessToken != null &&
-                    tournamentId != null &&
-                    userFromIdToken != null
-                  ) {
-                    _onTournamentSubmit(auth?.accessToken, {
-                      name: userFromIdToken.name,
-                      email: userFromIdToken.email,
-                      profileImage: userFromIdToken.picture,
-                      tournamentRef: {
-                        id: tournamentId,
-                      },
-                    });
-                  }
-                }}
-              >
-                Assign
-              </button>
-            </div>
-          ) : (
-            <>
-              <div className="card card-side bg-base-100 shadow-xl mt-10 w-full sm:w-96 p-3 sm:p-5 flex">
-                <figure className="rounded-none">
-                  <img
-                    src={currentUser.profileImage}
-                    alt={`Picture of ${currentUser.name}`}
-                    className="rounded-md border-darkGrey border-2"
-                  />
-                </figure>
-                <div className="text-start pl-5">
-                  <p className="text-navy dark:text-aqua font-ubuntuRegular  text-l sm:text-xl pb-2">
-                    {currentUser.name}
-                  </p>
-                  <p>{currentUser.email}</p>
-                  <p>Rating: {currentUser.rating}</p>
-                  <p>Games played: {currentUser.gamesPlayed}</p>
-                </div>
+            <select
+              className="mb-5 mr-5 select select-info w-full max-w-xs"
+              defaultValue={"DEFAULT"}
+              onChange={_onTournamentChange}
+            >
+              <option value={"DEFAULT"} disabled>
+                Select office
+              </option>
+              {tournamentList?.map((tournament) => {
+                return (
+                  <option key={tournament.id} value={tournament.id}>
+                    {tournament.name}
+                  </option>
+                );
+              })}
+            </select>
+            <button
+              className={`btn btn-ghost ${
+                tournamentId != null
+                  ? "btn-outline btn-success"
+                  : "btn-disabled"
+              }  ${isTournamentListLoading ? "loading" : ""}`}
+              onClick={() => {
+                if (
+                  auth?.accessToken != null &&
+                  tournamentId != null &&
+                  userFromIdToken != null
+                ) {
+                  _onTournamentSubmit(auth?.accessToken, {
+                    name: userFromIdToken.name,
+                    email: userFromIdToken.email,
+                    profileImage: userFromIdToken.picture,
+                    tournamentRef: {
+                      id: tournamentId,
+                    },
+                  });
+                }
+              }}
+            >
+              Assign
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="card card-side bg-base-100 shadow-xl mt-10 w-full sm:w-96 p-3 sm:p-5 flex">
+              <figure className="rounded-none">
+                <img
+                  src={currentUser.profileImage}
+                  alt={`Picture of ${currentUser.name}`}
+                  className="rounded-md border-darkGrey border-2"
+                />
+              </figure>
+              <div className="text-start pl-5">
+                <p className="text-navy dark:text-aqua font-ubuntuRegular  text-l sm:text-xl pb-2">
+                  {currentUser.name}
+                </p>
+                <p>{currentUser.email}</p>
+                <p>Rating: {currentUser.rating}</p>
+                <p>Games played: {currentUser.gamesPlayed}</p>
               </div>
-              <GamesHistory />
-            </>
-          )}
-        </div>
+            </div>
+            <GamesHistory />
+          </>
+        )}
       </>
     </ProtectedRoute>
   );
