81e06dbc25fabd9e7d109b4d506c03e198a4a442
IvaTsu <ivan.tsud@tretton37.com>
2023-04-12 16:00:20 +0200

    build: install and configure 'vite-plugin-svgr'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // override path's fill color so that it can be changed via 'color' prop
+        replaceAttrValues: {
+          "#539165": "currentColor",
+          "#E11D48": "currentColor",
+        },
+      },
+    }),
+  ],
 });
