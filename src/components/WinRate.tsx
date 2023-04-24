6947b1e1cb29c27df422fc9f64669199c77d80d2
IvaTsu <ivan.tsud@tretton37.com>
2023-04-24 12:41:34 +0200

    feat(RatingTable): display 'winRate' tab instead of 'office'

---
+++ src/components/WinRate.tsx
@@ -0,0 +1,30 @@
+import { useMemo } from "react";
+
+export const WinRate = ({ value }: { value: number }): JSX.Element => {
+  const winRateColorGradation = useMemo(() => {
+    if (value <= 0.3) {
+      return "text-red";
+    }
+    if (value > 0.3 && value <= 0.5) {
+      return "text-orange";
+    }
+    if (value > 0.5 && value <= 0.7) {
+      return "text-yellow";
+    }
+    if (value > 0.7 && value <= 0.9) {
+      return "text-green";
+    }
+    if (value > 0.9 && value <= 1) {
+      return "text-aqua";
+    }
+    return "";
+  }, [value]);
+
+  return (
+    <span
+      className={`bg-cloudBirst p-2 font-semibold dark:bg-none ${winRateColorGradation}`}
+    >
+      {Math.round(value * 100)}%
+    </span>
+  );
+};
