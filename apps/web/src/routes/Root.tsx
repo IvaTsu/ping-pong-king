1a94c0661c1961eb1882300549f4f5cdecdb0963
IvaTsu <ivan.tsud@gmail.com>
2025-08-07 14:15:32 +0200

    refactor: remove unnecessary component 'ChooseOfficeDropdown'

--- apps/web/src/routes/Root.tsx
+++ apps/web/src/routes/Root.tsx
@@ -1,4 +1,3 @@
-import { ChooseOfficeDropdown } from "../components/ChooseOfficeDropdown";
 import NavigationBar from "../components/NavigationBar";
 import { RatingTable } from "../components/tables/RatingTable";
 import ProtectedRoute from "../routes/ProtectedRoute";
